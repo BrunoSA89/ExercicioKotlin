2ecdfa39b951a23c3b8905ebc533ee92bfb37e79
bruno <bruno.otavio.1203@gmail.com>
2022-12-26 12:05:49 -0300

    "Primeiro commit"

---
+++ app/src/main/java/com/example/exercicio_kotlin_01/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.exercicio_kotlin_01
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import com.example.exercicio_kotlin_01.databinding.ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        chargeTextView()
+        viewBinding()
+    }
+
+    private fun view () {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+    }
+    private fun chargeTextView() {
+        val list = listOf( "Bruno", "Caio", "Paulo")
+        var currentItemIndex = 0
+
+        binding.BotaoNext.setOnClickListener() {
+            if (currentItemIndex >= list.size) {
+                currentItemIndex = 0
+            }
+            binding.textView.text = list(currentItemIndex)
+            currentItemIndex++
+        }
+
+    }
+
+
+
+}
